84ca2a00653c78f445d6613e24ece29b08b51af1
LylahL <leimin304@gmail.com>
2024-03-02 14:59:15 -0700

    css

--- frontEnd/speech-recognition/src/VoiceButtons.tsx
+++ frontEnd/speech-recognition/src/VoiceButtons.tsx
@@ -15,17 +15,16 @@ const VoiceButtons: React.FC<VoiceButtonsProps> = ({ setSelectedVoice }) => {
         <div style={{ padding: '20px' }}>
             <div className={styles1.mainView}>
                 <div>
-                  <h4>Select a voice to start</h4>
+                  <h4>Select a Speaker to start</h4>
                 </div>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' , height: '35vh'}}>
-              <div style={ {display: 'flex', justifyContent: 'center', alignItems: 'center'} }>
-              <button className = {styles.response} onClick={() => handleVoiceSelection('Voice 1')}>Voice 1</button>
-                <button className = {styles.response} onClick={() => handleVoiceSelection('Voice 2')}>Voice 2</button>
+              <div style={ {display: 'flex'} }>
+              <button className = {styles.response} onClick={() => handleVoiceSelection('Speaker 1')}>Speaker 1</button>
+                <button className = {styles.response2} onClick={() => handleVoiceSelection('Speaker 2')}>Speaker 2</button>
               </div>
               </div>             
         </div>
     );
 };
-
 export default VoiceButtons;
